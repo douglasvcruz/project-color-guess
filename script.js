71934539b42859b22ccfb34b947142dd754d808a
Douglas <cbdouglasvirissimo@hotmail.com>
2022-11-29 23:15:31 -0300

    4º Requisito

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const rgb = () => {
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
   return `rgb(${r},${g},${b})`;
-}
+};
 
 const ballGenerator = () => {
   for (let i = 0; i < 6; i += 1) {
