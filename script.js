995861f33ea4d5252670fde0f174d86190bf68a2
Douglas <cbdouglasvirissimo@hotmail.com>
2022-11-30 17:11:43 -0300

    5º, 6º e 7º Requisito

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const balls = document.getElementById('section-ball');
+const rgbColor = document.getElementById('rgb-color');
+const reset = document.getElementById('reset-game');
+const answer = document.getElementById('answer');
+const score = document.getElementById('score');
 
 const rgb = () => {
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
   return `rgb(${r},${g},${b})`;
+};
+
+const incrementScore = () => {
+  score.innerHTML = 3 + Number(score.innerHTML);
 };
 
 const ballGenerator = () => {
@@ -14,6 +22,32 @@ const ballGenerator = () => {
     ball.style.backgroundColor = rgb();
     balls.appendChild(ball);
   }
+  const circle = document.querySelectorAll('.ball');
+  rgbColor.innerText = circle[Math.floor(Math.random() * 5)].style.backgroundColor;
 };
 
 ballGenerator();
+
+const correctBall = () => {
+  const circle = document.querySelectorAll('.ball');
+
+  for (let i = 0; i < circle.length; i += 1) {
+    circle[i].addEventListener('click', (e) => {
+      if (e.target.style.backgroundColor === rgbColor.innerHTML) {
+        answer.innerHTML = 'Acertou!';
+        incrementScore();
+      } else {
+        answer.innerHTML = 'Errou! Tente novamente!';
+      }
+    });
+  }
+};
+
+const resetButton = () => {
+  answer.innerHTML = 'Escolha uma cor';
+  balls.innerHTML = '';
+  ballGenerator();
+  correctBall();
+};
+
+reset.addEventListener('click', resetButton);
