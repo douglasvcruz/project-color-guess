a7a32780dc4bf8379c6daa21eca4c029f5abe7a6
Douglas <cbdouglasvirissimo@hotmail.com>
2022-11-29 23:14:52 -0300

    4º Requisito

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const balls = document.getElementById('section-ball');
 
-// function rgb() {
-//   const r = Math.floor(Math.random() * 255);
-//   const g = Math.floor(Math.random() * 255);
-//   const b = Math.floor(Math.random() * 255);
-//   return `rgb(${r},${g},${b})`;
-// }
+const rgb = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r},${g},${b})`;
+}
 
 const ballGenerator = () => {
   for (let i = 0; i < 6; i += 1) {
     const ball = document.createElement('div');
     ball.className = 'ball';
+    ball.style.backgroundColor = rgb();
     balls.appendChild(ball);
   }
 };
