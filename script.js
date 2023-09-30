4d098eabd532086f2f7fb16535beadd66c2b1c86
Douglas <cbdouglasvirissimo@hotmail.com>
2023-09-30 16:24:17 -0300

    feat: trying new functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const balls = document.getElementById('section-ball');
+const circles = document.getElementById('section-circle');
 const rgbColor = document.getElementById('rgb-color');
 const reset = document.getElementById('reset-game');
 const answer = document.getElementById('answer');
@@ -15,41 +15,43 @@ const incrementScore = () => {
   score.innerHTML = 3 + Number(score.innerHTML);
 };
 
-const ballGenerator = () => {
+const generateCircles = () => {
+  circles.innerHTML = '';
   for (let i = 0; i < 6; i += 1) {
-    const ball = document.createElement('div');
-    ball.className = 'ball';
-    ball.style.backgroundColor = rgb();
-    balls.appendChild(ball);
+    const circle = document.createElement('div');
+    circle.className = 'circle';
+    circle.style.backgroundColor = rgb();
+    circles.appendChild(circle);
   }
-  const circle = document.querySelectorAll('.ball');
+  const circle = document.querySelectorAll('.circle');
   rgbColor.innerText = circle[Math.floor(Math.random() * 5)].style.backgroundColor;
 };
 
-ballGenerator();
+generateCircles();
 
-const correctBall = () => {
-  const circle = document.querySelectorAll('.ball');
-
+const correctCircle = () => {
+  const circle = document.querySelectorAll('.circle');
   for (let i = 0; i < circle.length; i += 1) {
     circle[i].addEventListener('click', (e) => {
-      if (e.target.style.backgroundColor === rgbColor.innerHTML) {
+      if (e.target.style.backgroundColor === rgbColor.innerText) {
         answer.innerHTML = 'Acertou!';
         incrementScore();
       } else {
         answer.innerHTML = 'Errou! Tente novamente!';
       }
+      generateCircles();
+      correctCircle()
     });
   }
 };
 
-correctBall();
+correctCircle();
 
 const resetButton = () => {
   answer.innerHTML = 'Escolha uma cor';
-  balls.innerHTML = '';
-  ballGenerator();
-  correctBall();
+  circles.innerHTML = '';
+  generateCircles();
+  correctCircle();
 };
 
 reset.addEventListener('click', resetButton);
