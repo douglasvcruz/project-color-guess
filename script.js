46f3b74a02b6ad186cde2c1751c1b86196ab043e
Douglas <cbdouglasvirissimo@hotmail.com>
2023-09-30 16:29:07 -0300

    fix: lint

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ const generateCircles = () => {
     circles.appendChild(circle);
   }
   const circle = document.querySelectorAll('.circle');
-  rgbColor.innerText = circle[Math.floor(Math.random() * 5)].style.backgroundColor;
+  rgbColor.innerText =
+    circle[Math.floor(Math.random() * 5)].style.backgroundColor;
 };
 
 generateCircles();
